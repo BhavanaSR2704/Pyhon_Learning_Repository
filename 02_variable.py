5a96c345c5fca0d818c714dbbf4950313f29184b
BhavanaSR2704 <bhavanasr272006@gmail.com>
2025-11-02 23:53:25 +0530

    Create 02_variable.py

---
+++ 02_variable.py
@@ -0,0 +1,7 @@
+#VARIABLE IS A NAME GIVEN TO A MEMORY LOCATION IN A PROGRAM / VARIABLE WHICH STORES A VALUE
+name=input("\nEnter your name:")             # name=variable and input=value
+age=int(input("Enter your age:"))
+print("I'm ",name)
+print("My age is:",age)
+
+print(type(name))   #USED TO CHECK THE DATA TYPE BY TYPE FUNCTION
