2ffb991dfd5cf5b7e2d850be02d77c234c0c995b
BhavanaSR2704 <bhavanasr272006@gmail.com>
2025-11-02 23:40:34 +0530

    Update and rename 0.1_python to 01_basic.py

---
+++ 01_basic.py
@@ -0,0 +1,13 @@
+# boy_name=input("Enter the name: ")
+# girl_name=input("Enter the name: ")
+# print(boy_name ,"and" ,girl_name,"are friends")
+
+
+a="101"
+b="110"
+c=int(a)
+d=int(b)
+e=c+d
+f=str(e)
+print(f)
+print(type(f))
