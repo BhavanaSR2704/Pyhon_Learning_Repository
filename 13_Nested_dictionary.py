f5873ad7ea74a4b148dd6cc2379dffe58f9f36f2
BhavanaSR2704 <bhavanasr272006@gmail.com>
2025-11-07 00:00:43 +0530

    13_Nested_dictionary.py

---
+++ 13_Nested_dictionary.py
@@ -0,0 +1,15 @@
+#A nested dictionary means a dictionary inside another dictionary
+#SYNTAX
+# nested_dict = {
+#     key1: {keyA: valueA, keyB: valueB},
+#     key2: {keyC: valueC, keyD: valueD}
+# }
+students = {
+    "student1": {"name": "Bhavana", "age": 19, "course": "ISE"},
+    "student2": {"name": "Bobby", "age": 20, "course": "CSE"}
+}
+print(students)  #------O/P={
+#  'student1': {'name': 'Bhavana', 'age': 19, 'course': 'ISE'},
+#  'student2': {'name': 'Bobby', 'age': 20, 'course': 'CSE'}
+# }
+
