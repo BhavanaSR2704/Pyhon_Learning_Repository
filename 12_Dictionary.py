09108a279de3f64ba424ace51bcfc8c36338a917
BhavanaSR2704 <bhavanasr272006@gmail.com>
2025-11-06 23:57:26 +0530

    12_Dictionary.py

---
+++ 12_Dictionary.py
@@ -0,0 +1,14 @@
+# A dictionary is a collection of data in key–value pairs
+#SYNTAX
+# dictionary_name = {
+#     key1: value1,
+#     key2: value2,
+#     key3: value3
+# }
+student = {
+    "name": "Bhavana",
+    "age": 19,
+    "course": "Information Science"
+}
+print(student)         #-------------------------O/P = {'name': 'Bhavana', 'age': 19, 'course': 'Information Science'}
+
