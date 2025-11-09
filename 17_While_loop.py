13bd6c60395fa8d6641e9fba5ff69364cf070ac1
BhavanaSR2704 <bhavanasr272006@gmail.com>
2025-11-09 22:59:17 +0530

    17_While_loop.py

---
+++ 17_While_loop.py
@@ -0,0 +1,18 @@
+#While loop is used to repeat instractions
+#SYNTAX
+#while condition:
+    # code to execute
+i=0
+while i<=10:
+    print(i)
+    i+=1       
+#--------------------------------
+
+i=1
+while i<=10:
+    x=0
+    while x<i:
+        print("Bhanu",end=" ")
+        x+=1
+    print("")
+    i+=1 
