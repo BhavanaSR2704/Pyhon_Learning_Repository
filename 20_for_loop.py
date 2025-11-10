b57f55552bb2b604fe5f3c096ca7166dbd063166
BhavanaSR2704 <bhavanasr272006@gmail.com>
2025-11-10 22:54:31 +0530

    20_for_loop.py

---
+++ 20_for_loop.py
@@ -0,0 +1,10 @@
+#For loop is used for sequential traversal,for traversing list,string,tuple etc..
+#SYNTAX
+#for variable in sequence:
+    # code to execute
+for i in range(1, 6):
+    print(i)
+#----------------------------
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
