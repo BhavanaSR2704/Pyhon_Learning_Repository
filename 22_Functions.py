a3d10297c83518467479deefc083b7c4544647c4
BhavanaSR2704 <bhavanasr272006@gmail.com>
2025-11-11 23:54:52 +0530

    22_Functions.py

---
+++ 22_Functions.py
@@ -0,0 +1,21 @@
+#Block of statements that perform a specific task
+#We use functions because if the code redunant number of times
+#SYNTAX
+#def function_name(parameters):
+    # block of code
+    #return value
+def add_numbers(a, b):
+    sum = a + b
+    print("Sum is:", sum)
+add_numbers(5, 10)   #------------O/P Sum is: 15
+
+
+#----------------------------------------------------
+
+def multiply(a, b):
+    return a * b
+result = multiply(4, 5)
+print("Result is:", result)   #------------O/P Result is: 20
+
+
+
