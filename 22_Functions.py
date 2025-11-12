bea63f9001677f8ba111738821bcf785ec5f0c24
BhavanaSR2704 <bhavanasr272006@gmail.com>
2025-11-12 22:35:54 +0530

    22_Functions.py

--- 22_Functions.py
+++ 22_Functions.py
@@ -17,5 +17,31 @@ def multiply(a, b):
 result = multiply(4, 5)
 print("Result is:", result)   #------------O/P Result is: 20
 
+#------------------------------------------------------
+#USING  *args AND **kwargs
+#*args
+# It allows the function to take any number of arguments (even none).Inside the function, args becomes a tuple containing all the passed values.
+
+def avg(*args):
+    if len(args)==0:
+        return 0
+    return sum(args)/len(args)
+print(avg(10,20,30))                #----------------------O/P 20.0
+
+
+#**kwargs
+#It pass any number of keyword arguments (i.e., arguments with names).
+
+def student_info(**details):    #USING **kwargs
+    for key, value in details.items():
+        print(f"{key}: {value}")
+
+student_info(name="Anand", age=22, course="Python")
+
+#OUT PUT
+#----------------
+# name: Anand
+# age: 22
+# course: Python
 
 
