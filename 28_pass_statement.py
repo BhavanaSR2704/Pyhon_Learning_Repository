35c7f5280169fd3ec28031383d38216740cc352b
BhavanaSR2704 <bhavanasr272006@gmail.com>
2025-11-14 23:43:06 +0530

    28_pass_statement.py

---
+++ 28_pass_statement.py
@@ -0,0 +1,31 @@
+# PASS IS A NULL STATEMENT THAT DOES NOTHING.IT IS USED AS A PLACE HOLDER FOR FUTURE CODE
+#FOR EXAMPLE
+for i in range(5):                #------O/P ERROR
+#---------------------------
+for i in range(5):
+  pass                            #-----O/P NO ERROR
+#------------------------------------------
+
+age = 18
+if age >= 18:
+    pass   # I might add permission code later
+else:
+    print("Not allowed")
+#-----------------------------------------------
+
+def add_student():
+    pass  # code will be added later
+def view_students():
+    print("Showing all students...")
+view_students()
+add_student()
+
+#------------------------------------------\
+
+students = ["Rahul", "Priya", "Vivek", "Unknown"]
+for name in students:
+    if name == "Unknown":
+        pass     # do nothing for Unknown
+    else:
+        print("Present:", name)
+
