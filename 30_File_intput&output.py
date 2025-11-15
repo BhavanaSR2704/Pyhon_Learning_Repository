f470296e858c0ea34a5c386a9a4893ea88d517d5
BhavanaSR2704 <bhavanasr272006@gmail.com>
2025-11-15 23:56:16 +0530

    30_File_intput&output.py

--- 30_File_intput&output.py
+++ 30_File_intput&output.py
@@ -1,1 +1,23 @@
+word="learning"
+with open("practice.txt","r")as f:
+    data=f.read()
+    if(data.find(word)!=-1):
+        print("Found")
+    else:
+        print("Not Find")           
 
+#OUTPUT
+#Found
+
+#------------------------------------------------
+
+word="Bhavana"
+with open("practice.txt","r")as f:
+    data=f.read()
+    if(data.find(word)!=-1):
+        print("Found")
+    else:
+        print("Not Find")
+
+#OUTPUT
+#Not Found
