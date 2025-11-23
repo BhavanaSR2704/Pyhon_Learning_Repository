f0561f8c0b9cd6936804417aab4fad3c2941a6f2
BhavanaSR2704 <bhavanasr272006@gmail.com>
2025-11-23 17:25:13 +0530

    36_class_pac2.py

--- 36_class_pac2.py
+++ 36_class_pac2.py
@@ -1,1 +1,15 @@
+#Static Methods
+#Method that not uses the self parameter 
+class Student :
+  @staticmethod 
+  def college():
+    print("ABC College")
 
+#----------------------------------------------------------------------------
+#Decorators allow us to wrap another function in order to extend the behaviour of the wrapped functions without permanently modifying it
+def hello():
+  print("hello")
+@staticmethod
+s1.hello()
+
+#---------O/P :- hello
