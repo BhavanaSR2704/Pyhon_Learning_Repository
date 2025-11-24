63d4c211a78799b6fdbd6ddc233a057a3b89eb8c
BhavanaSR2704 <bhavanasr272006@gmail.com>
2025-11-24 23:45:37 +0530

    37_Abtraction.py

---
+++ 37_Abtraction.py
@@ -0,0 +1,21 @@
+# Hiding the implementation details of a class and only showing the essential features to the user 
+class Car:
+    def __init__(self):
+        self.acc = False
+        self.brk = False
+        self.clutch = False
+
+    def start(self):
+        self.clutch = True
+        self.acc = True
+        print("Car started...")
+
+# creating objects
+car1 = Car()
+car2 = Car()
+
+# calling start() for car2
+car2.start()
+
+#----------------------------O/P
+#Car started...
