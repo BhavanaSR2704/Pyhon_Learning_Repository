10d695019b1d6eed902358bfb86fce15dac60502
BhavanaSR2704 <bhavanasr272006@gmail.com>
2025-11-29 23:53:20 +0530

    44_login_decorator.py

--- 44_login_decorator.py
+++ 44_login_decorator.py
@@ -1,1 +1,19 @@
+def login_required(func):
+    def wrapper():
+        user = input("Enter username: ")
+        password = input("Enter password: ")
+
+        if user == "Bhavana" and password == "1234":
+            print("Login successful!")
+            func()
+        else:
+            print("Login failed! Access denied.")
+    return wrapper
+
+
+@login_required
+def dashboard():
+    print("Welcome to the Dashboard")
+
+dashboard()
 
