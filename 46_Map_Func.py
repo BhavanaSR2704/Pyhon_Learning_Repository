1c2674df88d17c4d6f053f9c7a68f9339c37bcb2
BhavanaSR2704 <bhavanasr272006@gmail.com>
2025-11-30 21:49:31 +0530

    46_Map_Func.py

---
+++ 46_Map_Func.py
@@ -0,0 +1,13 @@
+nums=[1,2,3,4,5]
+res=map(lambda x:x*2,nums)
+print(list(res))
+
+#====================O/P:-[2, 4, 6, 8, 10]
+
+#adding two num using map func
+a=[1,2,3,4,5]
+b=[6,7,8,9,10]
+res=map(lambda x,y:x+y,a,b)
+print(list(res))
+
+#=================O/P:-[7, 9, 11, 13, 15]
