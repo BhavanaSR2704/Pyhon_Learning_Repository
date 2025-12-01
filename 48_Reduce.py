bc574a6cd2dd6eddcec72c0abe06495eaf08fc4f
BhavanaSR2704 <bhavanasr272006@gmail.com>
2025-12-01 21:10:06 +0530

    48_Reduce.py

--- 48_Reduce.py
+++ 48_Reduce.py
@@ -1,1 +1,11 @@
+from functools import reduce
 
+numbers = [1, 2, 3, 4, 5]
+
+result = reduce(lambda a, b: a + b, numbers)
+
+print(result)
+
+#IT REDUCES THE LIST AND GIVE A SINGLE VALUE
+
+#=======================O/P:-15
