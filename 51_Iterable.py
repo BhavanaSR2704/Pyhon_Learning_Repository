d0d41611fa5925c34b3be5aa47675d6799d357c0
BhavanaSR2704 <bhavanasr272006@gmail.com>
2025-12-04 23:06:07 +0530

    51_Iterable.py

--- 51_Iterable.py
+++ 51_Iterable.py
@@ -1,1 +1,6 @@
-
+nums=[1,2,3,4]   #ITERABLE---LIST,TUPLE
+# for num in nums:           #IF WE USE FOR LOOP IT GIVE ALL AT A ONCE
+#     print(num)      
+it=iter(nums)           #ITERATOR
+print(next(it)) #print first number only                  
+print(next(it)) # print second number.......                     #ITERATING/TRAVERSING
