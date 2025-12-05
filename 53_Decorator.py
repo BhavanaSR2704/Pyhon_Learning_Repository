87bbf1ecf02dea3af2505895ac1196dee9610c9c
BhavanaSR2704 <bhavanasr272006@gmail.com>
2025-12-06 00:15:28 +0530

    53_Decorator.py

--- 53_Decorator.py
+++ 53_Decorator.py
@@ -1,1 +1,13 @@
+def greet_decorator(func):
+    def wrapper():
+        print("Hello!")
+        func()
+        print("Welcome to Python decorators")
+    return wrapper
+
+@greet_decorator
+def say_name():
+    print("My name is Bhavana")
+
+say_name()
 
