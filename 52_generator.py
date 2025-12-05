2d6ab999a70e6acfee6e6102659ef5b541f19b37
BhavanaSR2704 <bhavanasr272006@gmail.com>
2025-12-05 23:04:40 +0530

    52_generator.py

--- 52_generator.py
+++ 52_generator.py
@@ -1,1 +1,18 @@
+#ALL GENERATORS ARE ITERABLES
+#IT IS ALSO USED TO REDUCE THE MEMORY SPACE
+import sys
 
+
+l=[x*x for x in range(1,1001)]                           #MEMORY SPACE=8856
+print(type(l))
+print(sys.getsizeof(l))
+# for i in l:
+#     print(i)
+
+
+
+gl=(x*x for x in range(1,1001))                          #MEMORY SPACE=200
+print(type(gl))
+print(sys.getsizeof(gl))
+# for i in gl:
+#     print(i)
