7bae3423da30f1bfa390968b69436ed333d2caa0
BhavanaSR2704 <bhavanasr272006@gmail.com>
2025-12-08 23:10:32 +0530

    55_Comprehension.py

--- 55_Comprehension.py
+++ 55_Comprehension.py
@@ -8,3 +8,12 @@ print(dm)
 
 #-----------------------------------------------------------
 
+students=["B","H","Z"]
+marks=["1","2","3"]
+d={}
+for i in range(3): #if we wants to calculate the add list then add len()
+    d[students[i]]=marks[i]
+print(d)
+
+#===============================O/P:-
+{'B': '1', 'H': '2', 'Z': '3'}
