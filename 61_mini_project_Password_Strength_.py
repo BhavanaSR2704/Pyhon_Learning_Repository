74373a7b25f2af9b0855d357a99727b71e903d4e
BhavanaSR2704 <bhavanasr272006@gmail.com>
2025-12-15 21:22:18 +0530

    61_mini_project_Password_Strength_.py

--- 61_mini_project_Password_Strength_.py
+++ 61_mini_project_Password_Strength_.py
@@ -1,1 +1,22 @@
+password = input("Enter a password: ")
+
+strength = 0
+
+if len(password) >= 8:
+    strength += 1
+if any(char.isdigit() for char in password):
+    strength += 1
+if any(char.isupper() for char in password):
+    strength += 1
+if any(char.islower() for char in password):
+    strength += 1
+if any(char in "!@#$%^&*" for char in password):
+    strength += 1
+
+if strength <= 2:
+    print("Weak Password")
+elif strength == 3:
+    print("Medium Password")
+else:
+    print("Strong Password")
 
