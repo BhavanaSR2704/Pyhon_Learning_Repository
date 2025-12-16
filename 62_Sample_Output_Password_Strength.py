b8d0bc2d800502c36c20bc58da2bfdadbd1c1fec
BhavanaSR2704 <bhavanasr272006@gmail.com>
2025-12-16 23:07:47 +0530

    62_Sample_Output_Password_Strength.py

--- 62_Sample_Output_Password_Strength.py
+++ 62_Sample_Output_Password_Strength.py
@@ -1,1 +1,11 @@
+#============================O/P:-1
+Enter a password: name
+Weak Password
 
+#============================O/P:-2
+Enter a password: name1111
+Medium Password
+
+#============================O/P:-3
+Enter a password: name@1111
+Strong Password
