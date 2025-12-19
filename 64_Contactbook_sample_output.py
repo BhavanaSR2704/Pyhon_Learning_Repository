ae3c85becc6619f7659312f59857144b77e61d93
BhavanaSR2704 <bhavanasr272006@gmail.com>
2025-12-19 21:40:32 +0530

    64_Contactbook_sample_output.py

--- 64_Contactbook_sample_output.py
+++ 64_Contactbook_sample_output.py
@@ -1,1 +1,30 @@
 
+1. Add Contact
+2. View Contacts
+3. Search Contact
+4. Exit
+Enter choice: 1
+Name: Bhavana
+Phone: 1234567891
+Contact saved 
+
+1. Add Contact
+2. View Contacts
+3. Search Contact
+4. Exit
+Enter choice: 2
+Bhavana : 1234567891
+
+1. Add Contact
+2. View Contacts
+3. Search Contact
+4. Exit
+Enter choice: 3
+Enter name to search: Bhavana
+Phone: 1234567891
+
+1. Add Contact
+2. View Contacts
+3. Search Contact
+4. Exit
+Enter choice: 4
